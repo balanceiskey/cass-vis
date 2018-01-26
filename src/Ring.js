24aadd6d159fe7a982d73005d2e446eca961303b
Mason Donahue <masond+github@gmail.com>
2018-01-26 17:46:19 -0600

    Add barebones Ring component

---
+++ src/Ring.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+//var PieChart = require('react-d3-basic').PieChart;
+class Ring extends Component {
+  render() {
+    return (
+      <div className="ring">lol</div>
+    )
+  }
+}
